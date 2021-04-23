841fa21f0e3402eb569a4aaa6204d938f66db93a
‘zhou <anthony0716@hotmail.com>
2021-04-24 00:18:17 +1000

    to be continued

--- script.js
+++ script.js
@@ -10,4 +10,30 @@ $(document).ready(()=>{
         $('.navbar .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
     });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        autoHeight:true,
+        responsive:{
+            0:{
+                items:1,
+                nav: false
+            },
+            500:{
+                items:1,
+                nav: false
+            },
+            800:{
+                items:2,
+                nav: false
+            },
+            1200:{
+                items:3,
+                nav: false
+            }
+        }
+    });
 })
